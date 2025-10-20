15319ef89a1a4211e71744c915a6e8353aa1e2f2
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-10-20 02:02:49 +0000

    feat:  storageBucket: "studio-7242321923-1100a.firebasestorage.app"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "studio-7242321923-1100a.firebaseapp.com",
   projectId: "studio-7242321923-1100a",
-  storageBucket: "studio-7242321923-1100a.appspot.com",
+  storageBucket: "studio-7242321923-1100a.firebasestorage.app",
   messagingSenderId: "290803328759",
   appId: "1:290803328759:web:edc5579bfb58dcd58215f2",
 };
